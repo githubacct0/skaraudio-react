d088d66dab79af382d027b9aa3281e0e3fb4844f
Kevin Schlenker <kevin@skaraudio.com>
2022-02-19 13:22:33 -0500

    --
    Signed-off-by: Kevin Schlenker <kevin@skaraudio.com>

--- src/components/Header.client.jsx
+++ src/components/Header.client.jsx
@@ -17,8 +17,11 @@ export default function Header({collections, storeName}) {
   return (
     <>
       <header
-        className="sticky top-0 left-0 right-0 z-20 w-full bg-slate-700 transition-[height] ease-in-out duration-500"
+        className="sticky top-0 left-0 right-0 z-20 w-full transition-[height] ease-in-out duration-500"
         role="banner"
+        style={{
+          backgroundColor: 'rgb(49,53,57)'
+        }}
       >
         <div
           className={`px-6 md:px-8 md:py-6 lg:pt-4 lg:pb-0 mx-auto ${
