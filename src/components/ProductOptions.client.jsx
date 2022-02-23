40d3d586b22619066a70a98f9e05989ee550f057
githubacct0 <githubacct0@outlook.com>
2022-02-23 21:39:43 +0100

    - menu work/design

--- src/components/ProductOptions.client.jsx
+++ src/components/ProductOptions.client.jsx
@@ -23,9 +23,9 @@ export default function ProductOptions() {
       {options.map(({name, values}) => {
         return (
           <fieldset key={name} className="mt-8">
-            <legend className="mb-4 text-xl font-medium text-gray-900">
-              {name}
-            </legend>
+            {/*<legend className="mb-4 text-xl font-medium text-gray-900">*/}
+            {/*  {name}*/}
+            {/*</legend>*/}
             <div className="flex items-center flex-wrap gap-4 mb-4">
               {values.map((value) => {
                 const checked = selectedOptions[name] === value;
